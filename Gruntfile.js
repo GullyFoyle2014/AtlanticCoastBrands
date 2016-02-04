2adbc43c873db57a27ddc112e4ffd38c78e5681f
GullyFoyle2014 <alex.m2004@gmail.com>
2016-02-04 17:08:30 -0500

    adding fonts, initial test js and css files testing grunt config and setting up for dev.

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,40 +27,18 @@ module.exports = function(grunt) {
     concat: {
       js: {
         src: 'js/*.js',
-        dest: 'js/concat/acbTest_cat.min.js'
+        dest: 'js/concat/acbTest_cat.js'
       },
       css: {
         src: 'css/compiled/*.css',
         dest: 'css/compiled/concat/acbTest_cat.css'
       }
-    },
-    cssmin: {
-      target: {
-        files: [{
-          expand: true,
-          cwd: 'css/compiled/concat',
-          src: ['acbTest.css', '!*.min.css'],
-          dest: 'css/compiled/concat/min',
-          ext: '.min.css'
-        }]
-      }
-    },
-    uglify: {
-      options: {
-        mangle: false
-      },
-      my_target: {
-        files: {
-          'js/concat/min/acbTest_cat.min.js': ['js/concat/acbTest_cat.js']
-        }
-      }
     }
+
   });
   // Each plugin must be loaded following this pattern
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.registerTask('default', ['jshint','sass','concat','cssmin','uglify']); 
+  grunt.registerTask('default', ['jshint','sass','concat']); 
 };
---
+++ js/acbTest.js
@@ -0,0 +1,6 @@
+//AtlanticCoastBrands Test JS
+$( document ).ready(function() {
+
+	$('body h1').addClass('mainHd');
+
+});//end doc.ready
