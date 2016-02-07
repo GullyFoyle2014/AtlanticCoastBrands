ae6d02838cd5b3a76593bd75eeba13e4fcd2bf20
GullyFoyle2014 <alex.m2004@gmail.com>
2016-02-06 19:49:24 -0500

    adding last page elements in, adding countdown timer script, still need a cleanup round, maybe some animations

--- js/concat/acbTest_cat.js
+++ js/concat/acbTest_cat.js
@@ -1,6 +1,24 @@
 //AtlanticCoastBrands Test JS
 $( document ).ready(function() {
 
-	$('body h1').addClass('mainHd');
+	function countDown(){
+		$('#clock').countdown('2016/02/09 12:00:00')
+		 .on('update.countdown', function(event) {
+			var format = '%H:%M:%S';
+			if(event.offset.days > 0) {
+				format = '%-d day%!d ' + format;
+		   }
+		   if(event.offset.weeks > 0) {
+				format = '%-w week%!w ' + format;
+		 	}
+		 	$(this).html(event.strftime(format));
+		 })
+			.on('finish.countdown', function(event) {
+				$(this).html('This offer has expired!')
+				.parent().addClass('disabled');
+		});
+	}//end countDown
+
+	countDown();
 
 });//end doc.ready
